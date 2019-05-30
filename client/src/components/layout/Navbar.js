6dbdfbd0ef4bc3a85c01cfe723eb856b706a92bf
Jordan Paz <jordandavidpaz@gmail.com>
2019-05-30 10:26:48 -0700

    Set up routing, add navbar, landing, register, and login components

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return (
-    <nav className="navbar bg-dark">
-      <h1>
-        <a href="index.html">Home</a>
-      </h1>
-      <ul>
-        <li>Shop</li>
-        <li>Cart</li>
-        <li>Account</li>
-      </ul>
-    </nav>
-  );
+  return <div>Navbar</div>;
 };
 
 export default Navbar;
