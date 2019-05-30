22e69be403667af25b3b98f87028557ec3b8d475
Jordan Paz <jordandavidpaz@gmail.com>
2019-05-30 12:05:18 -0700

    Add useState hook to register component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="name"
+          name="name"
+          value={name}
+          onChange={e => onChange(e)}
+          required
+        />
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          value={email}
+          onChange={e => onChange(e)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="password"
+          name="password"
+          value={password}
+          onChange={e => onChange(e)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="password2"
+          name="password2"
+          value={password2}
+          onChange={e => onChange(e)}
+          required
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Register;
